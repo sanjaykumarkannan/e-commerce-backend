fa972d17668e33e1ca6f9bd8a72232d3078e16b9
sanjaykumarkannan <sanjaykumarkannan@gmail.com>
2021-08-13 02:01:06 +0530

    Committing query methods

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,20 @@
 package com.luv2code.ecommerce.dao;
 
+
+import java.math.BigDecimal;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
-
+	
+	Page<Product> findByCategoryId(@RequestParam ("id") Long id,Pageable pageable);
+	
+	Page<Product> findByUnitPrice(@RequestParam("unitPrice") BigDecimal unitPrice,Pageable pageable); 
 }
